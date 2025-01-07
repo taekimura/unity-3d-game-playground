a9a4059baf244a184ebec8019164bafd8afdbba5
Tae Kimura <sheepman7893@gmail.com>
2025-01-06 21:42:11 -0800

    update: add camera follow logic

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform lookAtTarget;
+
+    [SerializeField]
+    private Transform followTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(lookAtTarget);
+
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        transform.position = followTarget.position;
+    }
+}
