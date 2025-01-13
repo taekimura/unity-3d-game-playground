2867d09d0a88ef353a35a07d13f46ae309a1b1f3
Tae Kimura <sheepman7893@gmail.com>
2025-01-12 21:25:12 -0800

    update: add Turret

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if(other.tag == "Damage")
+        // {
+        //     TakeDamage();
+        // }
+
+        if(other.tag == "Damage")
+        {
+            other.GetComponent<MeshRenderer>().material.color = Color.blue;
+            GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Damage")
+        {
+            other.GetComponent<MeshRenderer>().material.color = Color.red;
+            GetComponent<MeshRenderer>().material.color = Color.blue;
+        }
+    }
+
+    private void TakeDamage()
+    {
+        Debug.Log("Player hit!");
+    }
+}
