ceb09b62b24203be5c540dcdc6164944da85767e
Tae Kimura <sheepman7893@gmail.com>
2025-01-13 18:26:59 -0800

    update: add Shooting turret feature

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rigidbody;
+
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Vector3 direction;
+
+    private float lifeTime = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody.velocity = direction * speed;
+
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject projectilePrefab;
+
+    [SerializeField]
+    private GameObject[] gunBarrels;
+
+    [SerializeField]
+    private Animator animator;
+
+    public void Shoot(int index)
+    {
+        Instantiate(projectilePrefab, gunBarrels[index].transform.position, Quaternion.identity);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            animator.SetBool("Shoot", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            animator.SetBool("Shoot", false);
+        }
+    }
+}
