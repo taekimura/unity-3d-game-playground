d7d8e6b751df14667f8c7280c0a1e8accb4c1820
Tae Kimura <sheepman7893@gmail.com>
2025-01-13 21:38:45 -0800

    fix: add Car

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private GameObject[] wheels;
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject wheel in wheels)
+        {
+            wheel.transform.Rotate(Vector3.right* rotationSpeed * Time.deltaTime, Space.Self);
+        }
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Portal")
+        {
+            transform.position = startPos;
+        }
+    }
+}
