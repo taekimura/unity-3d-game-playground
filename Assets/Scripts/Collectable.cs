db4391fef83b9ebf9765080986242ed1659f34e9
Tae Kimura <sheepman7893@gmail.com>
2025-01-16 21:14:00 -0800

    update: add gears to the environment as collectables

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+
+    [SerializeField]
+    private float rotationSpeed;
+
+    [SerializeField]
+    private Vector3 rotationDirection;
+
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private Vector3 movementDirection;
+
+    [SerializeField]
+    private float distance;
+
+    private float startY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        Move();
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(rotationDirection * rotationSpeed * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        transform.Translate(movementDirection * movementSpeed * Time.deltaTime, Space.World);
+    
+        if (Mathf.Abs(transform.position.y - startY) >= distance)//When we have reached our goal
+        {
+            startY = transform.position.y; //Log new start postion
+            movementDirection *= -1; //reverse direction
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
