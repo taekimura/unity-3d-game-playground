45062bfc6eb2ac7f2428e8de7cdddb5a20433abe
Tae Kimura <sheepman7893@gmail.com>
2025-01-19 19:29:26 -0800

    fix: use coroutines for speechbubbles

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class NPC : MonoBehaviour
@@ -12,10 +13,18 @@ public class NPC : MonoBehaviour
     [SerializeField]
     private CanvasGroup canvasGroup;
 
+    [SerializeField]
+    private TMP_Text bubbleText;
+
+    [SerializeField]
+    private float speechSpeed;
+
+    [SerializeField]
+    private string conversation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -24,11 +33,48 @@ public class NPC : MonoBehaviour
         
     }
 
+    private void Speak()
+    {
+        StartCoroutine(FadeIn());
+        StartCoroutine(WriteText(conversation));
+    }
+
+    private IEnumerator WriteText(string data)
+    {
+        bubbleText.text = string.Empty;
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            bubbleText.text += data[i];
+            yield return new WaitForSeconds(speechSpeed);
+        }
+    }
+
+    private IEnumerator FadeIn()
+    {
+        while (canvasGroup.alpha < 1)
+        {
+            canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, 1, Time.deltaTime * 5);
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeOut(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        while (canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, 0, Time.deltaTime * 5);
+            yield return null;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            canvasGroup.alpha = 1;
+            
             animator.Play("Wave_NPC");
         }
     }
@@ -37,8 +83,9 @@ public class NPC : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            canvasGroup.alpha = 0;
-            animator.Play("Wave_NPC");
+            animator.Play("Wave_Bye_NPC");
+            StartCoroutine(WriteText("Bye!"));
+            StartCoroutine(FadeOut(2));
         }
     }
 }
